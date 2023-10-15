3ec2dcc0a98849721d878d283e3b6054b7c0ce41
kuragna <abourrich@proton.me>
2023-10-15 20:45:53 +0200

    cd command

--- src/builtins/ms_cd.c
+++ src/builtins/ms_cd.c
@@ -6,104 +6,90 @@
 /*   By: aabourri <aabourri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 19:24:40 by glacroix          #+#    #+#             */
-/*   Updated: 2023/10/12 19:33:28 by aabourri         ###   ########.fr       */
+/*   Updated: 2023/10/15 20:41:53 by aabourri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	ms_var_pos(t_env *env, char *var)
-{
-	size_t i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(var);
-	while (i < env->lenght && env->vars[i])
-	{
-		if (ft_strncmp(env->vars[i], var, len) == 0)
-		{
-			return (i);
-		}
-		i += 1;
-	}
-	return (-1);
-}
-
 int	ms_cd(t_env *env, char *path)
 {
-	char	*str;
-	int	err;
-	int	i;
-	int	j;
-
-	if (ft_strncmp(path, "-", 1) == 0)
+	char *go;
+	size_t	len;
+	
+	// TODO: replace go by path
+	go = path;
+	len = ft_strlen(path);
+	if (*path == '\0')
 	{
-		j = ms_var_pos(env, "OLDPWD");
-		if (j == -1)
-		{
-			MS_ERROR("cd: OLDPWD not set", "", "");
-			return (1);
-		}
-		path = &env->vars[j][7];
+		go = ms_getenv(env, "HOME=");
+		if (go == NULL)
+			return ms_error ("cd: HOME not set\n");
+		//go = &env->vars[pos][5];
+		go += 5;	
 	}
-	if (!*path)
+	else if (ft_strncmp(path, "-", len) == 0 || ft_strncmp(path, "--", len) == 0)
 	{
-		i = ms_var_pos(env, "HOME=");
-		if (i == -1)
-		{
-			MS_ERROR("cd: HOME not set", "", "");
-			return (1);
-		}
-		path = &env->vars[i][5];
+		//pos = ms_get_idx(env, "OLDPWD=", 7);
+		go = ms_getenv(env, "OLDPWD=");
+		if (go == NULL)
+			ms_error("cd: OLDPWD not set\n");
+		//go = &env->vars[pos][7];		
+		go += 7;
 	}
-	err = chdir(path);
-	if (err == -1)
+	if ((*path == '~' && *(path + 1) == '\0') || (*path == '~' && *(path + 1) == '/'))
 	{
-		printf("cd: %s: %s\n", path, strerror(errno));
-		return (1);
+		path += 1;
+		//pos = ms_get_idx(env, "HOME=",  5);
+		go = ms_getenv(env, "HOME=");
+		if (go == NULL)
+			return ms_error("cd: HOME not set\n");
+		//go = ft_strjoin(&env->vars[pos][5], path);
+		go = ft_strjoin(go + 5, path);
 	}
-
-	/* check if function return -1 */
-	i = ms_var_pos(env, "PWD=");
-	j = ms_var_pos(env, "OLDPWD=");
-
-	str = &env->vars[i][4]; //getcwd(NULL, 0); // check error
-	ft_strlcpy(&env->vars[j][7], &env->vars[i][4], ft_strlen(&env->vars[i][3]));
-	ft_strlcpy(&env->vars[i][4], str, ft_strlen(str) + 1);
-	return (0);
+	if (chdir(go) == -1)
+	{
+		return ms_error("cd: %s: %s\n", go, strerror(errno));
+	}
+	//pos = ms_get_idx(env, "PWD=", 4);
+	go = ms_getenv(env, "PWD=");
+	//ms_export(env, ft_strjoin("OLD", env->vars[pos]));
+	ms_export(env, ft_strjoin("OLD", go));
+	ms_export(env, ft_strjoin("PWD=", getcwd(NULL, 0)));
+	return 0;
 }
+
+#if 0
+
 
 int	main(int argc, char **argv, char **envp)
 {
+	atexit(ms_leaks);
 	(void) argc;
 	(void) argv;
 	(void) envp;
 	
 	t_env env = env_dup(envp);
 
-	(void)env;
-
-	size_t i;
-
-	i = 0;
-
-	ms_cd(&env, "");
-	
-	if (fork() == 0)
-	{
-		char *path = "/bin/pwd";
-		char *args[] = {
-			"pwd",
-			NULL,
-		};
-		int err = execve(path, args, env.vars);
-		if (err == -1)
-		{
-			perror("Error");
-		}
-	}
-	waitpid(-1, NULL, 0);
-	
+	ms_cd(&env, "/"); // go to root
+	ms_pwd();
+	ms_cd(&env, "~"); // go home
+	ms_pwd();
+	ms_cd(&env, "-"); // go back to root
+	ms_pwd();
+	ms_cd(&env, "--"); // go back to home
+	ms_pwd();
+	ms_cd(&env, "../"); // go to previes dir
+	ms_pwd();
+	ms_cd(&env, ""); // go home
+	ms_pwd();
+	//ms_env(env);
 	return 0;
 }
+#endif //
+
+
+
+
+
+
